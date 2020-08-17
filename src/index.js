0bde44d759b089431239f038f39301c124249f6c
Luke Chen <luke.chen.2010@gmail.com>
2020-08-16 20:12:18 -0500

    Commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
+const passageList = document.querySelector('#passage_list');
+
 function getPassages() 
 {
-    return fetch('http://localhost:3000/passages', {mode:'no-cors'})
-      .then(res => res.json())
+    return fetch('http://localhost:3000/passages')
+      .then(response => response.json())
+}
+
+function getHighscore(passageId) 
+{
+    return fetch('http://localhost:3000/passages/' + passageId + '/highscores')
+      .then(response => response.json())
 }
 
 function renderPassages(passage) 
 {
-    console.log(passage.title)
+    let passageContainer = document.createElement("div"); 
+    passageContainer.className = "passage_container";
+
+    let highscoreCount = document.createElement("span"); 
+    highscoreCount.className = "highscore_count";
+
+    let passageTitle = document.createElement("span"); 
+    passageTitle.className = "passage_title";
+    
+    getHighscore(passage.id).then(highscore =>
+        highscoreCount.innerHTML = highscore.length
+    );
+
+    passageTitle.innerHTML = passage.title;
+    
+    passageContainer.appendChild(highscoreCount);
+    passageContainer.appendChild(passageTitle);
+    passageList.appendChild(passageContainer);
 }
 
 getPassages().then(passages => 
 {
     passages.forEach(passage => 
     {
-        renderPassages(passage)
+        renderPassages(passage);
     })
 })
 
